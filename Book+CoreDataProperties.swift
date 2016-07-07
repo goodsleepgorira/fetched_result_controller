756b46901c59dbe794617d9746f6074fe282d76c
goodsleepgorira <goodsleepgorira@yahoo.co.jp>
2016-07-07 06:00:20 -0500

    Test

--- Book+CoreDataProperties.swift
+++ Book+CoreDataProperties.swift
@@ -1,12 +1,5 @@
 //
 //  Book+CoreDataProperties.swift
-//  TestFetchRequest
-//
-//  Created by 齋藤緒 on 2016/07/07.
-//  Copyright © 2016年 TestOrganization. All rights reserved.
-//
-//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
-//  to delete and recreate this implementation file for your updated model.
 //
 
 import Foundation
